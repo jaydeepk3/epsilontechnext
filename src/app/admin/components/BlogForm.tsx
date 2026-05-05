a05dfdc46c3eed6a948cbf2eba78c6657d460c82
Jaydeep Kataria <jaydeepkataria@gmail.com>
2026-05-05 16:32:05 +0530

    fix issue

--- src/app/admin/components/BlogForm.tsx
+++ src/app/admin/components/BlogForm.tsx
@@ -89,7 +89,7 @@ export default function BlogForm({ blog }: { blog?: any }) {
                             id="slug"
                             required
                             defaultValue={blog?.slug}
-                            pattern="^[a-z0-9-]+$"
+                            pattern="^[a-z0-9\-]+$"
                             title="Only lowercase letters, numbers, and hyphens are allowed"
                             className="w-full px-4 py-2 border border-slate-300 rounded-lg focus:ring-2 focus:ring-sky-500 focus:border-sky-500 transition shadow-sm bg-slate-50 focus:bg-white"
                             placeholder="e.g. top-app-developers-2026"
